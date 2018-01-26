758bcb7ad40917697606bf790775206a74867bdd
陈锦庭 <495177767@qq.com>
2018-01-26 22:28:38 +0800

    Move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,14 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
+    
     protected $fillable = [
         'name', 'email', 'password',
     ];
